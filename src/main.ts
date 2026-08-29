2b16ace1930ba3020aa32eabd1cbd5f41c2cfcec
Ariane Bouthillette <1838720@csfoy.ca>
2026-08-29 19:59:47 -0400

    avancement css du formulaire

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import './style.css';
-//Déclaration de constantes
+//Déclaration navigation étapes
 const bouton1 = document.querySelector('.bouton1');
 const bouton2 = document.querySelector('.bouton2');
 const bouton3 = document.querySelector('.bouton3');
@@ -7,6 +7,12 @@ const etape1 = document.querySelector('.etape1');
 const etape2 = document.querySelector('.etape2');
 const etape3 = document.querySelector('.etape3');
 const etape4 = document.querySelector('.etape4');
+
+//Déclaration navigation "retour";
+const boutonRetour1 = document.querySelector('.boutonRetour1');
+const boutonRetour2 = document.querySelector('.boutonRetour2');
+
+//Déclaration modification des trucs 
 
 const boutonEnvoie=document.querySelector('.boutonEnvoie');
 document.documentElement.classList.add('js');
@@ -24,7 +30,7 @@ initialisation();
 //Fonction pour la navigation entre les étapes
 function navigationEtapes() {
   //Navigation etape1 -> etape2
-  bouton1?.addEventListener("click", (event) => {
+  bouton1?.addEventListener("click", () => {
     etape1?.classList.add("hidden");
     bouton1?.classList.add("hidden");
     etape2?.classList.remove("hidden");
@@ -32,14 +38,14 @@ function navigationEtapes() {
   });
 
   //Navigation etape2 -> etape3
- bouton2?.addEventListener("click", (event) => {
+ bouton2?.addEventListener("click", () => {
     etape2?.classList.add("hidden");
     bouton2?.classList.add("hidden");
     etape3?.classList.remove("hidden");
     bouton3?.classList.remove("hidden");
   });
 //Navigation etape3 -> etape4
-  bouton3?.addEventListener("click", (event) => {
+  bouton3?.addEventListener("click", () => {
     etape3?.classList.add("hidden");
     bouton3?.classList.add("hidden");
     etape4?.classList.remove("hidden");
@@ -49,3 +55,24 @@ function navigationEtapes() {
 }
 
 navigationEtapes();
+
+//Fonction de navigation "retour" 
+function navigationRetour() {
+  //Navigation étape 2 -> étape 1
+  boutonRetour1?.addEventListener("click", () => {
+      etape2?.classList.add("hidden");
+      bouton2?.classList.add("hidden");
+      etape1?.classList.remove("hidden");
+      bouton1?.classList.remove("hidden");
+  })
+  //Navigation étape 3 -> étape 2 
+  boutonRetour2?.addEventListener("click",(event) => {
+      etape3?.classList.add("hidden");
+      bouton3?.classList.add("hidden");
+      etape2?.classList.remove("hidden");
+      bouton2?.classList.remove("hidden");
+  });
+}
+navigationRetour();
+
+//Fonction modification de section 
